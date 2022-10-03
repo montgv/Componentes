a4f7d4d7a6ee2f7861a2e11ae6aa3f9aa08f9b7d
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-03 09:07:45 +0200

    Añadimos Spinner personalizado

--- app/src/main/java/com/example/componentes/MainActivity.java
+++ app/src/main/java/com/example/componentes/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.example.componentes.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private ActivityMainBinding binding;
 
@@ -19,16 +21,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        String[] opciones = {"Opción 1", "Opción 2"};
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                //android.R.layout.simple_spinner_item, opciones); creacion del spinner
+
+        //personalizar el spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opciones_spinner, R.layout.mi_spinner);
+        adapter.setDropDownViewResource(R.layout.mi_spinner);
+        binding.spinner.setAdapter(adapter);
+
         binding.rB1.setOnClickListener(this);
         binding.rB2.setOnClickListener(this);
+        
+        binding.spinner.setOnItemSelectedListener(this);
     }
-
-
+    
     @Override
     public void onClick(View view) {
         if (binding.rB1.isChecked()) {
             Toast.makeText(this, "Opción 1", Toast.LENGTH_SHORT).show();
-        } else {
+        } else if (binding.rB2.isChecked()){
             Toast.makeText(this, "Opción 2", Toast.LENGTH_SHORT).show();
         }
     }
@@ -48,5 +61,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 }
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String Opcion = (String) adapterView.getItemAtPosition(i);
+        
+        if (Opcion.equals("Opción 1")) {
+            Toast.makeText(this, "Pulsada opción 1", Toast.LENGTH_SHORT).show();
+        } else if (Opcion.equals("Opción 2")) {
+            Toast.makeText(this, "Pulsada opción 2", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        
     }
 }
